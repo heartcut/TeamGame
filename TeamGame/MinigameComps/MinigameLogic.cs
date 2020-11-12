2bd6758160236d844444fc34ae2bf4162c76c9b5
jesse <dameronjesse@gmail.com>
2020-11-12 05:17:09 -0600

    getting the var generation working right within the game components to properly use the db vars for input in the answers

--- TeamGame/MinigameComps/MinigameLogic.cs
+++ TeamGame/MinigameComps/MinigameLogic.cs
@@ -18,7 +18,7 @@ namespace TeamGame.MinigameComps
         //you can do some kind of wait stuff idk but it works for now
         protected override void OnInitialized()
         {
-            //below is what i will use to get my game and vars
+            //takes the position of the variable from the db and makes it a 6
             sna[Var1] = "6";
             sna[Var2] = "6";
         }
@@ -29,7 +29,6 @@ namespace TeamGame.MinigameComps
             "9","9","9","9","9","9","9","9",
             "9","9","9","9","9","9","9","9"
         };
-        
         public string status;
         public int rightanswer = 0;
         public int wronganswer = 0;
@@ -37,6 +36,7 @@ namespace TeamGame.MinigameComps
         {
             if (sna[a] == "6")
             {
+                sna[a] = "9";
                 if (rightanswer == 1)
                 {
                     status = "youwin!";
@@ -53,19 +53,6 @@ namespace TeamGame.MinigameComps
                 status = wronganswer + " wronganswer" + rightanswer + " right answer";
             }
         }
-        public static int[] RandomSixesV2()
-        {
-            Random rnd = new Random();
-            int[] wheresixes = new int[3];
-            int first = rnd.Next(32);
-            int second = rnd.Next(32);
-            wheresixes[0] = 1;
-            wheresixes[1] = first;
-            wheresixes[2] = second;
-            return wheresixes;
-
-            
-        }
     }
 
     public partial class MemorizeColorNumber
@@ -73,13 +60,39 @@ namespace TeamGame.MinigameComps
 
         //this is the onload overridded method
         //you can do some kind of wait stuff idk but it works for now
+        string firstcolor;
+        string secondcolor;
+        string thirdcolor;
         protected override async Task OnInitializedAsync()
         {
+            //below sets the var from the db to something useable within the code
+            //in this case its setting the first answer colors
+            //return in the form of [color-color-color,num-num-num,whetherqiscolorornum-colans-numans,posofanswer-fakeanswer-fakeanswer]
+            switch (Var1.ToString().Substring(0, 1))
+            {
+                case "1": firstcolor = "red"; break;
+                case "2": firstcolor = "blue"; break;
+                case "3": firstcolor = "yellow"; break;
+                case "4": firstcolor = "green"; break;
+            }
+            switch (Var1.ToString().Substring(1, 1))
+            {
+                case "1": secondcolor = "red"; break;
+                case "2": secondcolor = "blue"; break;
+                case "3": secondcolor = "yellow"; break;
+                case "4": secondcolor = "green"; break;
+            }
+            switch (Var1.ToString().Substring(2, 1))
+            {
+                case "1": thirdcolor = "red"; break;
+                case "2": thirdcolor = "blue"; break;
+                case "3": thirdcolor = "yellow"; break;
+                case "4": thirdcolor = "green"; break;
+            }
+            ///todo finish this up and fix the checkanswer below
+            //todo
             //get rid of the below
-            PickColorNums();
             MemorizeStopWatch();
-            PickQ();
-            PickAnswers();
         }
 
         int memorizetimer = 2;
@@ -89,7 +102,7 @@ namespace TeamGame.MinigameComps
             is_stopwatchrunning = true;
             while (is_stopwatchrunning)
             {
-                await Task.Delay(1000);
+                await Task.Delay(5000);
                 if (is_stopwatchrunning)
                 {
                     memorizetimer = memorizetimer - 1;
@@ -100,26 +113,6 @@ namespace TeamGame.MinigameComps
 
         //1 is game state 2 is correct 3 is wrong
         public int MCNGameState = 1;
-        Random rndm = new Random();
-        //below is picking that question to ask either color or number
-        public bool ranbefore = false;
-        public string con;
-        public void PickQ()
-        {
-            if (!ranbefore)
-            {
-                if (rndm.Next(1, 3) == 1)
-                {
-                    con = "color";
-                    ranbefore = true;
-                }
-                else
-                {
-                    con = "number";
-                    ranbefore = true;
-                }
-            }
-        }
         //picking thw answer optoins
         public string mcnanswer;
         public string whichbox;
@@ -127,179 +120,6 @@ namespace TeamGame.MinigameComps
         public string answer2;
         public string answer3;
         public int whichisanswer;
-        public void PickAnswers()
-        {
-            if (con == "color")
-            {
-                //answer is a number
-                int whichcolor = rndm.Next(1, 4);
-                if (whichcolor == 1)
-                {
-                    mcnanswer = firstcolor;
-                    whichbox = "first";
-                }
-                else if (whichcolor == 2)
-                {
-                    mcnanswer = secondcolor;
-                    whichbox = "second";
-                }
-                else
-                {
-                    //whichcolor is 3
-                    mcnanswer = thirdcolor;
-                    whichbox = "third";
-
-                }
-            }
-            //i know its speghetti which all the if else but suck it
-            else
-            {
-                //answer is a number
-                int whichnumber = rndm.Next(1, 4);
-                if (whichnumber == 1)
-                {
-                    mcnanswer = firstnumber.ToString();
-                    whichbox = "first";
-                }
-                else if (whichnumber == 2)
-                {
-                    mcnanswer = secondnumber.ToString();
-                    whichbox = "second";
-                }
-                else
-                {
-                    //whichnumber is 3
-                    mcnanswer = thirdnumber.ToString();
-                    whichbox = "third";
-                }
-            }
-            //below i will populate variables to put in the button answers
-            //yea i know this is spaghetti too
-            whichisanswer = rndm.Next(1, 4);
-            if (whichisanswer == 1)
-            {
-                answer1 = mcnanswer;
-            }
-            else
-            {
-                if (con == "color")
-                {
-                    answer1 = colors[rndm.Next(0, 4)];
-                    while (answer1 == mcnanswer)
-                    {
-                        answer1 = colors[rndm.Next(0, 4)];
-                    }
-                }
-                else
-                {
-                    //is number
-                    answer1 = rndm.Next(0, 9).ToString();
-                    while (answer1 == mcnanswer.ToString())
-                    {
-                        answer1 = rndm.Next(0, 9).ToString();
-                    }
-                }
-            }
-            if (whichisanswer == 2)
-            {
-                answer2 = mcnanswer;
-            }
-            else
-            {
-                if (con == "color")
-                {
-                    answer2 = colors[rndm.Next(0, 4)];
-                    while (answer2 == answer1)
-                    {
-                        answer2 = colors[rndm.Next(0, 4)];
-                    }
-                }
-                else
-                {
-                    //is number
-                    answer2 = rndm.Next(0, 9).ToString();
-                    while (answer2 == answer1.ToString())
-                    {
-                        answer2 = rndm.Next(0, 9).ToString();
-                    }
-                }
-            }
-            if (whichisanswer == 3)
-            {
-                answer3 = mcnanswer;
-            }
-            else
-            {
-                if (con == "color")
-                {
-                    answer3 = colors[rndm.Next(0, 4)];
-                    while (answer3 == answer1 || answer3 == answer2)
-                    {
-                        answer3 = colors[rndm.Next(0, 4)];
-                    }
-                }
-                else
-                {
-                    //is number
-                    answer3 = rndm.Next(0, 9).ToString();
-                    while (answer3 == answer1.ToString() || answer3 == answer2)
-                    {
-                        answer3 = rndm.Next(0, 9).ToString();
-                    }
-                }
-            }
-        }
-        //picking the random colors and numbers
-        public bool picked = false;
-        public string firstcolor = "blue";
-        public string secondcolor = "blue";
-        public string thirdcolor = "blue";
-        public int firstnumber = 1;
-        public int secondnumber = 2;
-        public int thirdnumber = 3;
-        string[] colors = new string[4] { "blue", "red", "green", "yellow" };
-        public void PickColorNums()
-        {
-            if (!picked)
-            {
-                //gunna do some if else fuckery to get different for each which is what i did above for thw answers
-                firstcolor = colors[rndm.Next(0, 4)];
-                secondcolor = colors[rndm.Next(0, 4)];
-                if (secondcolor == firstcolor)
-                {
-                    while (secondcolor == firstcolor)
-                    {
-                        secondcolor = colors[rndm.Next(0, 4)];
-                    }
-                }
-                thirdcolor = colors[rndm.Next(0, 4)];
-                if (thirdcolor == firstcolor || thirdcolor == secondcolor)
-                {
-                    while (thirdcolor == firstcolor || thirdcolor == secondcolor)
-                    {
-                        thirdcolor = colors[rndm.Next(0, 4)];
-                    }
-                }
-                firstnumber = rndm.Next(0, 9);
-                secondnumber = rndm.Next(0, 9);
-                if (secondnumber == firstnumber)
-                {
-                    while (secondnumber == firstnumber)
-                    {
-                        secondnumber = rndm.Next(0, 9);
-                    }
-                }
-                thirdnumber = rndm.Next(0, 9);
-                if (thirdnumber == firstnumber || thirdnumber == secondnumber)
-                {
-                    while (thirdnumber == firstnumber || thirdnumber == secondnumber)
-                    {
-                        thirdnumber = rndm.Next(0, 9);
-                    }
-                }
-                picked = true;
-            }
-        }
         public void CheckAnswer(int a)
         {
             if (a == whichisanswer)
